3d14cce02d82ed9185c00b4c5f4080157eb69b61
Denny Huang <denny0223@gmail.com>
2019-09-08 14:37:25 +0800

    Update for PyCon

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 module.exports = {
+<<<<<<< HEAD
   spreadsheetKey: '1QYy_JGYPKoQat08ALT0FpGJt5SYnI3E3tlFoC6yUWlk',
+=======
+  spreadsheetKey: '1yfN0lD5WVMRmZpdHXvEFxyNYyyFoAa3jusftnnZRCmY',
+>>>>>>> f25ca7c (Update for PyCon)
 };
